42c01d987a5968da8c2744a181a83e49ffe8c7b1
oldmartijntje <oldmartijntje@gmai.com>
2022-04-07 09:48:57 +0200

    ez

--- zen.py
+++ zen.py
@@ -1,11 +1,7 @@
 import time
-file = open('README.md', 'r')
-read = file.read()
-splittedRead = read.split('\n')
-loop = 0
-while True:
-    print(splittedRead[loop])
-    time.sleep(1)
-    loop += 1
-    if loop == len(splittedRead):
-        loop = 0
+with open('README.md') as file:
+    line = file.readline() 
+    while line:
+        time.sleep(1)
+        line = file.readline()
+        print(line)
